e790f1b48416a5560cf1f6a1d8c5c21cf51a71d7
JOSE IVAN RIBEIRO DE OLIVEIRA <ivan_r.o@hotmail.com>
2020-08-06 22:20:01 -0300

    Rota main!

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import ClassesController from "./app/controllers/ClassesController";
 import ConnectionsController from "./app/controllers/ConnectionsController";
 
 const routes = express.Router();
 
+routes.get("/", (req: Request, res: Response) => {
+  return res
+    .status(200)
+    .json("Wellcome to proff's Api, it's runnig as expected...");
+});
 routes.post("/classes", ClassesController.create);
 routes.get("/classes", ClassesController.index);
 
